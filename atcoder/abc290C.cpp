cbb739c1a334219cdccd094a18fb2b9c1277f226
linkumokunn <126052859+kumokunnlin@users.noreply.github.com>
2023-02-28 19:43:53 +0800

    2023/02/28 update

---
+++ atcoder/abc290C.cpp
@@ -0,0 +1,54 @@
+/*
+=============================================================================
+      /\       \      /      /\         |\           /|    |===========
+     / 	\       \    /      /  \        | \         / |    |
+    /    \       \  /      /    \       |  \       /  |    |
+   /      \       |       /      \      |   \     /   |    |===========
+  /--------\      |      /--------\     |    \   /    |    |
+ /          \     |     /          \    |     \ /     |    |
+/            \    |    /            \   |      |      |    |===========
+     A        A
+  AAAAAAAA   AAA AAAA      A    A      A     A        A          A
+    A    A    A A    A     A     A     A      A      A A        A A
+   A     A    AA      A    A      A    A       A    A   A      A   A
+  A   A  A   AA     A      A  A   A    A  A    A
+ A     AAA    A    A       AAA         AAA               AAAAA
+=============================================================================
+*/
+#include<bits/stdc++.h>
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define F first
+#define S second
+#define ll long long
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define endl '\n'
+#define Orz ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MAXN 200001
+using namespace std;
+//YeedragOrz
+//8e7Orz
+//jassOrz
+//FHvirusOrz
+//samOrz
+//yungyaoOrz
+//littlecubeOrz
+//2qbingxuanOrz
+//FoodSheepOrz
+//ck_platypusOrz
+//koyingOrz
+//foxyyOrz
+//peiganOrz
+//jikuaiOrz
+
+int main(){
+	int n,k;cin>>n>>k;
+	int arr[n];for(int i=0;i<n;i++)cin>>arr[i];
+	int ans=0;
+	sort(arr,arr+n);
+	for(int i=0;i<n;i++)if(arr[i]==ans)ans++;
+	cout<<min(ans,k);
+    return 0;
+}
