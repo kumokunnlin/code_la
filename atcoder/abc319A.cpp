90864fea720e7fa61e4a6555ee7bb537ae9a2ca2
linki <126052859+kumokunnlin@users.noreply.github.com>
2023-09-09 20:28:37 +0800

    20230909 update

---
+++ atcoder/abc319A.cpp
@@ -0,0 +1,59 @@
+/*
+=============================================================================
+      /\       \      /      /\         |\           /|    |===========
+     / 	\       \    /      /  \        | \         / |    |
+    /    \       \  /      /    \       |  \       /  |    |
+   /      \       |       /      \      |   \     /   |    |===========
+  /--------\      |      /--------\     |    \   /    |    |
+ /          \     |     /          \    |     \ /     |    |
+/            \    |    /            \   |      |      |    |===========
+     A        A
+  AAAAAAAA   AAA AAAA      A    A      A     A        A          A
+    A    A    A A    A     A     A     A      A      A A        A A
+   A     A    AA      A    A      A    A       A    A   A      A   A
+  A   A  A   AA     A      A  A   A    A  A    A
+ A     AAA    A    A       AAA         AAA               AAAAA
+=============================================================================
+*/
+#include<bits/stdc++.h>
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define F first
+#define S second
+#define ll long long int
+#define pii pair<int,int>
+#define endl '\n'
+#define Orz ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define MAXN 200001
+using namespace std;
+//YeedragOrz
+//8e7Orz
+//jassOrz
+//FHvirusOrz
+//samOrz
+//yungyaoOrz
+//littlecubeOrz
+//2qbingxuanOrz
+//FoodSheepOrz
+//ck_platypusOrz
+//koyingOrz
+//foxyyOrz
+//peiganOrz
+
+int main(){
+	map<string,int> ma;
+	ma["tourist"]=3858;
+	ma["ksun48"]=3679;
+	ma["Benq"]=3658;
+	ma["Um_nik"]=3648;
+	ma["apiad"]=3638;
+	ma["Stonefeang"]=3630;
+	ma["ecnerwala"]=3613;
+	ma["mnbvmar"]=3555;
+	ma["newbiedmy"]=3516;
+	ma["semiexp"]=3481;
+	string s;cin>>s;
+	cout<<ma[s]<<endl;
+	return 0;
+}
